a0d36cb8846a9163ca1cb618b600209a44d6a3cb
farzadgo <farzadesign@gmail.com>
2023-09-04 09:57:03 +0200

    font family update without Inter

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
 import Head from 'next/head'
 // import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>moyo</title>
+        <title> moyo </title>
         <meta name="description" content="Moyo Bremen" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      {/* <main className={`${styles.main} ${inter.className}`}> */}
+      <main className={styles.main}>
         <div className={styles.message}>
-          <h2>moyo-bremen</h2>
-          <p>coming soon...</p>
+          <h2> moyo-bremen </h2>
+          <p> coming soon... </p>
         </div>
       </main>
     </>
